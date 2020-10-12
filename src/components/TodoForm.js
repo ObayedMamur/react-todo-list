96a3f777ac845b77f46660e1b5620f45b04c86f9
Obayed Mamur Zihan <obayedmamur@gmail.com>
2020-10-12 12:13:29 +0600

    Todo Form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,17 +1,34 @@
 import React, {useState} from "react";
+import uuid from "uuid";
 
-function TodoForm() {
+function TodoForm({ addTodo }) {
     const [todo, setTodo] = useState({
         id: "",
         task: "",
         completed: false
     });
 
+    function handleTaskInputChange(e) {
+        setTodo({...todo, task: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if(todo.task.trim()) {
+            addTodo({ ...todo, id: uuid.v4() });
+            setTodo({ ...todo, task: "" });
+        }
+    }
 
     return (
-    <form>
-        <input />
-        <button />
+    <form onSubmit={handleSubmit}>
+        <input
+            name="task"
+            type="text"
+            value={todo.task}
+            onChange={handleTaskInputChange}
+        />
+        <button type="submit" />
     </form>
     );
 }
